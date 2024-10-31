fcff30335fc62be767b9a2e53bb5266145b2d43f
Mustakim Billah Rafi <rafisharkar144@gmail.com>
2024-10-31 17:41:34 +0600

    Upload all major setups

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
+import Layout from './Layout'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <h1>Rafi Sharkar</h1>
+      <BrowserRouter>
+        <Layout/>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/Layout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Layout() {
+  return (
+    <div>
+        <h1>Rent On</h1>
+    </div>
+  )
+}
