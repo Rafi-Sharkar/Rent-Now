6de60504b6eab2a407f8158e93995e831627ff28
Mustakim Billah Rafi <rafisharkar144@gmail.com>
2024-11-02 13:15:11 +0600

    Set Range done

--- src/global_components/setrange/SetRange.jsx
+++ src/global_components/setrange/SetRange.jsx
@@ -3,6 +3,18 @@ import style from './setrange.module.css'
 
 export default function SetRange() {
   return (
-    <div>SetRange</div>
+    <>
+      <div className={style.srcontainer}>
+        <form>
+            <div className={style.srvalue}>
+              <input type="text" placeholder='Maximum' />
+              <input type="text" placeholder='Minimum' />
+            </div>
+            <div className={style.srsubmit}>
+              <input type="submit" value={'Set'} />
+            </div>
+        </form>
+      </div>
+    </>
   )
 }
