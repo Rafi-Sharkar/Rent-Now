1920efe926959279da0ebcddc18d7ecb7eac41e6
Mustakim Billah Rafi <rafisharkar144@gmail.com>
2024-11-15 17:13:32 +0600

    contact, service and about page completed

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,7 +1,117 @@
 import React from 'react'
+import style from './about.module.css'
+import rs1 from '../../assets/photos/Rafi_Sharkar.jpg'
 
 export default function About() {
   return (
-    <div>A</div>
+    <>
+      <div className={style.header}>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit fuga rerum ad ea eveniet necessitatibus incidunt autem iure, aut praesentium?</p>
+        <button>MEET US</button>
+      </div>
+      <div className={style.vision}>
+        <h2>Rent On</h2>
+        <div className={style.mivi}>
+          <div className={style.mivisub}>
+            <h3>Mission</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, aliquid nulla? Quis quia vel nihil at animi doloremque nam eveniet tenetur, dolorem nesciunt temporibus excepturi. Quos ducimus maxime vel itaque?</p>
+          </div>
+          <div className={style.mivisub}>
+            <h3>Vission</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, aliquid nulla? Quis quia vel nihil at animi doloremque nam eveniet tenetur, dolorem nesciunt temporibus excepturi. Quos ducimus maxime vel itaque?</p>
+          </div>
+        </div>
+      </div>
+      <div className={style.team}>
+        <h2>Team</h2>
+        <div className={style.teammates}>
+          <div className={style.teammate}>
+            <div className={style.tmimg}>
+              <img src={rs1} alt="image not found" />
+            </div>
+            <div className={style.tmdetail}>
+              <h3>CEO</h3>
+              <h1>Rafi Sharkar</h1>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit delectus dolorum neque! Fugit, necessitatibus deleniti!</p>
+
+            </div>
+            <div className={style.tmsl}>
+              <a href='https://www.facebook.com/rafi.sharkar.90/' ><i className="fa-brands fa-facebook hover:text-[#0866FF]"></i> </a>
+              <a href='##' ><i className="fa-brands fa-whatsapp hover:text-[#25d366]"></i> </a>
+              <a href='https://www.instagram.com/rafi_sharkar_0777?fbclid=IwY2xjawGjpCtleHRuA2FlbQIxMAABHRL6qSGeIRz-AWUy6I1vN60A5lPRHj8KmBVkbTufaTDoKkAPoDMlU79zEw_aem_i4JRhAcB1Bozx5eRootBsg' ><i className="fa-brands fa-instagram hover:text-[#e1306c]"></i> </a>
+              <a href='https://www.youtube.com/@rafi_sharkar_0777' ><i className="fa-brands fa-youtube hover:text-[#CD201F]"></i> </a>
+            </div>
+          </div>
+          <div className={style.teammate}>
+            <div className={style.tmimg}>
+              <img src={rs1} alt="image not found" />
+            </div>
+            <div className={style.tmdetail}>
+              <h3>CEO</h3>
+              <h1>Rafi Sharkar</h1>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit delectus dolorum neque! Fugit, necessitatibus deleniti!</p>
+
+            </div>
+            <div className={style.tmsl}>
+              <a href='https://www.facebook.com/rafi.sharkar.90/' ><i className="fa-brands fa-facebook hover:text-[#0866FF]"></i> </a>
+              <a href='##' ><i className="fa-brands fa-whatsapp hover:text-[#25d366]"></i> </a>
+              <a href='https://www.instagram.com/rafi_sharkar_0777?fbclid=IwY2xjawGjpCtleHRuA2FlbQIxMAABHRL6qSGeIRz-AWUy6I1vN60A5lPRHj8KmBVkbTufaTDoKkAPoDMlU79zEw_aem_i4JRhAcB1Bozx5eRootBsg' ><i className="fa-brands fa-instagram hover:text-[#e1306c]"></i> </a>
+              <a href='https://www.youtube.com/@rafi_sharkar_0777' ><i className="fa-brands fa-youtube hover:text-[#CD201F]"></i> </a>
+            </div>
+          </div>
+          <div className={style.teammate}>
+            <div className={style.tmimg}>
+              <img src={rs1} alt="image not found" />
+            </div>
+            <div className={style.tmdetail}>
+              <h3>CEO</h3>
+              <h1>Rafi Sharkar</h1>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit delectus dolorum neque! Fugit, necessitatibus deleniti!</p>
+
+            </div>
+            <div className={style.tmsl}>
+              <a href='https://www.facebook.com/rafi.sharkar.90/' ><i className="fa-brands fa-facebook hover:text-[#0866FF]"></i> </a>
+              <a href='##' ><i className="fa-brands fa-whatsapp hover:text-[#25d366]"></i> </a>
+              <a href='https://www.instagram.com/rafi_sharkar_0777?fbclid=IwY2xjawGjpCtleHRuA2FlbQIxMAABHRL6qSGeIRz-AWUy6I1vN60A5lPRHj8KmBVkbTufaTDoKkAPoDMlU79zEw_aem_i4JRhAcB1Bozx5eRootBsg' ><i className="fa-brands fa-instagram hover:text-[#e1306c]"></i> </a>
+              <a href='https://www.youtube.com/@rafi_sharkar_0777' ><i className="fa-brands fa-youtube hover:text-[#CD201F]"></i> </a>
+            </div>
+          </div>
+          <div className={style.teammate}>
+            <div className={style.tmimg}>
+              <img src={rs1} alt="image not found" />
+            </div>
+            <div className={style.tmdetail}>
+              <h3>CEO</h3>
+              <h1>Rafi Sharkar</h1>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit delectus dolorum neque! Fugit, necessitatibus deleniti!</p>
+
+            </div>
+            <div className={style.tmsl}>
+              <a href='https://www.facebook.com/rafi.sharkar.90/' ><i className="fa-brands fa-facebook hover:text-[#0866FF]"></i> </a>
+              <a href='##' ><i className="fa-brands fa-whatsapp hover:text-[#25d366]"></i> </a>
+              <a href='https://www.instagram.com/rafi_sharkar_0777?fbclid=IwY2xjawGjpCtleHRuA2FlbQIxMAABHRL6qSGeIRz-AWUy6I1vN60A5lPRHj8KmBVkbTufaTDoKkAPoDMlU79zEw_aem_i4JRhAcB1Bozx5eRootBsg' ><i className="fa-brands fa-instagram hover:text-[#e1306c]"></i> </a>
+              <a href='https://www.youtube.com/@rafi_sharkar_0777' ><i className="fa-brands fa-youtube hover:text-[#CD201F]"></i> </a>
+            </div>
+          </div>
+          <div className={style.teammate}>
+            <div className={style.tmimg}>
+              <img src={rs1} alt="image not found" />
+            </div>
+            <div className={style.tmdetail}>
+              <h3>CEO</h3>
+              <h1>Rafi Sharkar</h1>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit delectus dolorum neque! Fugit, necessitatibus deleniti!</p>
+
+            </div>
+            <div className={style.tmsl}>
+              <a href='https://www.facebook.com/rafi.sharkar.90/' ><i className="fa-brands fa-facebook hover:text-[#0866FF]"></i> </a>
+              <a href='##' ><i className="fa-brands fa-whatsapp hover:text-[#25d366]"></i> </a>
+              <a href='https://www.instagram.com/rafi_sharkar_0777?fbclid=IwY2xjawGjpCtleHRuA2FlbQIxMAABHRL6qSGeIRz-AWUy6I1vN60A5lPRHj8KmBVkbTufaTDoKkAPoDMlU79zEw_aem_i4JRhAcB1Bozx5eRootBsg' ><i className="fa-brands fa-instagram hover:text-[#e1306c]"></i> </a>
+              <a href='https://www.youtube.com/@rafi_sharkar_0777' ><i className="fa-brands fa-youtube hover:text-[#CD201F]"></i> </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
--- src/pages/service/service.jsx
+++ src/pages/service/service.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import style from './service.module.css'
+import { MdLocalLaundryService } from "react-icons/md";
 
 
 export default function Service() {
@@ -44,7 +45,24 @@ export default function Service() {
         </div>
       </div>
       <div className={style.service}>
-
+        <h2>Our Services</h2>
+        <div className={style.services}>
+          <div className={style.servicesub}>
+            <i className="fa-solid fa-truck-pickup"></i>
+            <h3>Pick Up</h3>
+            <p>Move your furniture safely</p>
+          </div>
+          <div className={style.servicesub}>
+            <MdLocalLaundryService className={style.serviceicon}/>
+            <h3>Laundry</h3>
+            <p>Get rid of the trouble of cleaning clothes</p>
+          </div>
+          <div className={style.servicesub}>
+            <i className="fa-solid fa-user-nurse"></i>
+            <h3>Plumber</h3>
+            <p>Move your furniture safely</p>
+          </div>
+        </div>
       </div>
     </>
   )
